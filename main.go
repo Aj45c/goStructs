7d9dc02a824ee2695d5c00855673608fe6360c14
Anthony <anthony.jose53@gmail.com>
2023-07-22 12:58:01 -0400

    starting to learn struct

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+type person struct { //no need to seprate things by comma or semi-colon.
+	//Since this is in the type struct curly braces go understnads what we are trying to do
+	firstName string
+	lastName  string
+}
+
+func main() {
+
+}
